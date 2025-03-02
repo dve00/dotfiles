3734b70ec45e5cf5db7c2d04a22cac682a1b29e1
Dario Veltri <dario.veltri@freiheit.com>
2025-03-02 20:02:30 +0100

    add flutter-tools

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -59,7 +59,59 @@ return {
       'stevearc/dressing.nvim', -- optional for vim.ui.select
       'nvim-telescope/telescope.nvim',
     },
-    config = true,
+    config = function()
+      require('flutter-tools').setup {
+        ui = {
+          -- the border type to use for all floating windows, the same options/formats
+          -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+          border = 'rounded',
+          -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+          -- please note that this option is eventually going to be deprecated and users will need to
+          -- depend on plugins like `nvim-notify` instead.
+        },
+        decorations = {
+          statusline = {
+            -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+            -- this will show the current version of the flutter app from the pubspec.yaml file
+            app_version = false,
+            -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+            -- this will show the currently running device if an application was started with a specific
+            -- device
+            device = true,
+            -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+            -- this will show the currently selected project configuration
+            project_config = false,
+          },
+        },
+        debugger = { -- integrate with nvim dap + install dart code debugger
+          enabled = false,
+          -- if empty dap will not stop on any exceptions, otherwise it will stop on those specified
+          -- see |:help dap.set_exception_breakpoints()| for more info
+          exception_breakpoints = {},
+          -- Whether to call toString() on objects in debug views like hovers and the
+          -- variables list.
+          -- Invoking toString() has a performance cost and may introduce side-effects,
+          -- although users may expected this functionality. null is treated like false.
+          evaluate_to_string_in_debug_views = true,
+          register_configurations = function(paths)
+            require('dap').configurations.dart = {
+              --put here config that you would find in .vscode/launch.json
+            }
+            -- If you want to load .vscode launch.json automatically run the following:
+            -- require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          enabled = true,
+          filter = nil, -- optional callback to filter the log
+          -- takes a log_line as string argument; returns a boolean or nil;
+          -- the log_line is only added to the output if the function returns true
+          notify_errors = false, -- if there is an error whilst running then notify the user
+          open_cmd = '15split', -- command to use to open the log buffer
+          focus_on_open = true, -- focus on the newly opened log window
+        },
+      }
+    end,
   },
   {
     'ThePrimeagen/harpoon',
